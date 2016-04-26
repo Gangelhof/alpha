acb55a3a607aefcbb0e457f520b4a52c84889410
pedskon <peter-riis@hotmail.com>
2016-04-26 14:43:50 +0200

    Update Stof.java

--- alpha/model/Stof.java
+++ alpha/model/Stof.java
@@ -1,5 +1,8 @@
 
+
 package alpha.model;
+
+import java.util.Random;
 
 
 public class Stof {
@@ -49,5 +52,58 @@ public class Stof {
     {
        this.tilgængelighed += antal;
     }
+    
+    public void Reberegn_stof()
+    {
+        Random randomizer;
+        randomizer = new Random();
+ 
+        int priceChance = randomizer.nextInt(100);
+        if (priceChance < 65) {
+            this.pris = calculateNewPrice(this.pris);
+        }
+        
+        int availabilityChance = randomizer.nextInt(100);
+        if (availabilityChance < 65) {
+            this.tilgængelighed = calculateNewAvailability(this.tilgængelighed);
+        }
+
+        
+    }
+    
+    private int calculateNewPrice(double currentPrice) {
+        Random randomizer;
+        randomizer = new Random();
+       
+        int upOrDown = randomizer.nextInt(2);
+        int changePercentage = randomizer.nextInt(85) + 1;
+        double newPrice;
+
+        if (upOrDown == 0) {
+            newPrice = currentPrice - (currentPrice * changePercentage / 100);
+        } else {
+            newPrice = currentPrice + (currentPrice * changePercentage / 100);
+        }
+
+        return (int) newPrice;
+    }
+    
+    private int calculateNewAvailability(int availability) {
+        Random randomizer;
+        randomizer = new Random();
+        
+        int upOrDown = randomizer.nextInt(2);
+        int changePercentage = randomizer.nextInt(40) + 16;
+        int newAvailability;
+
+        if (upOrDown == 0) {
+            newAvailability = (int) Math.floor(availability - (availability * changePercentage / 100));
+        } else {
+            newAvailability = (int) Math.floor(availability + (availability * changePercentage / 100));
+        }
+
+        return newAvailability;
+    }    
+   
     
 }//slut
